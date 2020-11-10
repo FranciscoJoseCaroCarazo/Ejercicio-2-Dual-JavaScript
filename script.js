8cda0e4bea0ec47e4ae23acce1eb809395a79aea
FranciscoJoseCaroCarazo <fcarocarazo@safareyes.es>
2020-11-10 17:43:03 +0100

    addLista terminado

--- script.js
+++ script.js
@@ -11,5 +11,45 @@
 window.addEventListener("load", onLoad);
 
 function onLoad() {
-  console.log("hi");
+  lista();
+  addLista();
+  console.log();
+}
+
+function lista() {
+  var spanSelecionado = document.querySelector(".selected");
+  var spanNode = spanSelecionado.parentNode;
+  var elemento2 = spanNode.previousSibling.previousSibling;
+
+  console.log(elemento2);
+}
+
+function addLista() {
+  //Obtenemos el id del elemento List2
+  var listNode = document.getElementById("list2");
+  //Creamos el boton 1, le asignamos la clase y el contenido del boton
+  var btn1 = document.createElement("Button");
+  btn1.innerHTML = "Texto Ejemplo 1";
+  btn1.classList.add("element-1");
+  //Introducimos el boton en el LI
+  var primerLi = document.createElement("Li");
+  primerLi.appendChild(btn1);
+  //Creamos el boton 2, le asignamos la clase y el contenido del boton
+  var btn2 = document.createElement("Button");
+  btn2.innerHTML = "Texto Ejemplo 3";
+  btn2.classList.add("element-3");
+  //Introducimos el boton en el LI
+  var segundoLi = document.createElement("Li");
+  segundoLi.appendChild(btn2);
+  //Creamos el boton 3, le asignamos la clase y el contenido del boton
+  var btn3 = document.createElement("Button");
+  btn3.innerHTML = "Texto Ejemplo 5";
+  btn3.classList.add("element-5");
+  //Introducimos el boton en el LI
+  var tercerLi = document.createElement("Li");
+  tercerLi.appendChild(btn3);
+  //Añadimos los diferentes LI a la lista
+  listNode.appendChild(primerLi);
+  listNode.appendChild(segundoLi);
+  listNode.appendChild(tercerLi);
 }
